ae367009c627b80346a5bdf07f62c553f0bd7aef
asjay <sejin1314@korea.ac.kr>
2024-05-06 10:54:59 +0900

    feat: work on home, committee page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,51 +1,18 @@
 import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
 import Container from '@mui/material/Container'
-import InputLabel from '@mui/material/InputLabel'
-import Link from '@mui/material/Link'
 import Stack from '@mui/material/Stack'
-import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
-import { styled } from '@mui/material/styles'
-import { visuallyHidden } from '@mui/utils'
-
-const StyledBox = styled('div')(({ theme }) => ({
-  alignSelf: 'center',
-  width: '100%',
-  height: 400,
-  marginTop: theme.spacing(8),
-  borderRadius: theme.shape.borderRadius,
-  boxShadow:
-    theme.palette.mode === 'light'
-      ? '0 0 12px 8px hsla(220, 25%, 80%, 0.2)'
-      : '0 0 24px 12px hsla(210, 100%, 25%, 0.2)',
-  outline: '1px solid',
-  backgroundImage: `url(${
-    theme.palette.mode === 'light'
-      ? '/static/images/templates/templates-images/hero-light.png'
-      : '/static/images/templates/templates-images/hero-dark.png'
-  })`,
-  backgroundSize: 'cover',
-  outlineColor:
-    theme.palette.mode === 'light'
-      ? 'hsla(220, 25%, 80%, 0.5)'
-      : 'hsla(210, 100%, 80%, 0.1)',
-  [theme.breakpoints.up('sm')]: {
-    marginTop: theme.spacing(10),
-    height: 700
-  }
-}))
 
 export default function Hero() {
   return (
     <Box
       id="hero"
-      sx={(theme) => ({
+      sx={() => ({
         width: '100%',
         backgroundImage:
-          theme.palette.mode === 'light'
-            ? 'radial-gradient(ellipse 80% 50% at 50% -20%, hsl(210, 100%, 90%), transparent)'
-            : 'radial-gradient(ellipse 80% 50% at 50% -20%, hsl(210, 100%, 16%), transparent)',
+          'url(/src/assets/images/ai_mobile_systems_head_img.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat'
       })}
     >
@@ -70,67 +37,16 @@ export default function Hero() {
               display: 'flex',
               flexDirection: { xs: 'column', sm: 'row' },
               alignItems: 'center',
-              fontSize: 'clamp(3rem, 10vw, 3.5rem)'
+              fontSize: 'clamp(3rem, 10vw, 3.5rem)',
+              color: 'common.white'
             }}
           >
-            Our&nbsp;latest&nbsp;
-            <Typography
-              component="span"
-              variant="h1"
-              sx={{
-                fontSize: 'inherit',
-                color: (theme) =>
-                  theme.palette.mode === 'light'
-                    ? 'primary.main'
-                    : 'primary.light'
-              }}
-            >
-              products
-            </Typography>
+            AI&nbsp;Mobile&nbsp;Systems
           </Typography>
-          <Typography
-            textAlign="center"
-            color="text.secondary"
-            sx={{ width: { sm: '100%', md: '80%' } }}
-          >
-            Explore our cutting-edge dashboard, delivering high-quality
-            solutions tailored to your needs. Elevate your experience with
-            top-tier features and services.
-          </Typography>
-          <Stack
-            direction={{ xs: 'column', sm: 'row' }}
-            spacing={1}
-            useFlexGap
-            sx={{ pt: 2, width: { xs: '100%', sm: 'auto' } }}
-          >
-            <InputLabel htmlFor="email-hero" sx={visuallyHidden}>
-              Email
-            </InputLabel>
-            <TextField
-              id="email-hero"
-              hiddenLabel
-              size="small"
-              variant="outlined"
-              aria-label="Enter your email address"
-              placeholder="Your email address"
-              inputProps={{
-                autoComplete: 'off',
-                'aria-label': 'Enter your email address'
-              }}
-            />
-            <Button variant="contained" color="primary">
-              Start now
-            </Button>
-          </Stack>
-          <Typography variant="caption" textAlign="center">
-            By clicking &quot;Start now&quot; you agree to our&nbsp;
-            <Link href="#" color="primary">
-              Terms & Conditions
-            </Link>
-            .
-          </Typography>
+          <Container
+            sx={{ width: '30%', height: '10px', bgcolor: 'primary.main' }}
+          ></Container>
         </Stack>
-        <StyledBox id="image" />
       </Container>
     </Box>
   )
