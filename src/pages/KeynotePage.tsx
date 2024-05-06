1863ba5f1ebe85e566cfd7ff772ea74c41bc64a9
hypnotes <hayeongpark@korea.ac.kr>
2024-05-06 11:17:59 +0900

    init

--- src/pages/KeynotePage.tsx
+++ src/pages/KeynotePage.tsx
@@ -1,32 +1,12 @@
 import Box from '@mui/material/Box'
-import Divider from '@mui/material/Divider'
-import FAQ from '../components/FAQ'
-import Features from '../components/Features'
-import Footer from '../components/Footer'
-import Hero from '../components/Hero'
-import Highlights from '../components/Highlights'
-import LogoCollection from '../components/LogoCollection'
-import Pricing from '../components/Pricing'
-import Testimonials from '../components/Testimonials'
+import KeyVisitors from '../components/KeyVisitors'
+import Footer from '@components/Footer'
 
 export default function KeynotePage() {
   return (
-    <>
-      <Hero />
-      <Box sx={{ bgcolor: 'background.default' }}>
-        <LogoCollection />
-        <Features />
-        <Divider />
-        <Testimonials />
-        <Divider />
-        <Highlights />
-        <Divider />
-        <Pricing />
-        <Divider />
-        <FAQ />
-        <Divider />
-        <Footer />
-      </Box>
-    </>
+    <Box sx={{ bgcolor: 'background.default' }}>
+      <KeyVisitors />
+      <Footer />
+    </Box>
   )
 }
