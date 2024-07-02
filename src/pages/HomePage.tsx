f5994eae3f9642a49069d1ba3db372d83f2c21d3
miridih-hypark <hypark@miridih.com>
2024-07-02 17:16:28 +0900

    TEST

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,75 @@
-import Introduction from '@components/Introduction'
-import Box from '@mui/material/Box'
-import Footer from '../components/Footer'
-import Hero from '../components/Hero'
+import { Column, ReactGrid, Row } from '@silevis/reactgrid'
+import '@silevis/reactgrid/styles.css'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Box, Typography, styled } from '@mui/material'
+
+interface Person {
+  name: string
+  surname: string
+}
+
+const getPeople = (): Person[] => [
+  { name: 'Thomas', surname: 'Goldman' },
+  { name: 'Susie', surname: 'Quattro' },
+  { name: '', surname: '' }
+]
+
+const getColumns = (): Column[] => [
+  { columnId: 'name', width: 150 },
+  { columnId: 'surname', width: 150 }
+]
+
+const headerRow: Row = {
+  rowId: 'header',
+  cells: [
+    { type: 'header', text: 'Name' },
+    { type: 'header', text: 'Surname' }
+  ]
+}
+
+const getRows = (people: Person[]): Row[] => [
+  headerRow,
+  ...people.map<Row>((person, idx) => ({
+    rowId: idx,
+    cells: [
+      { type: 'text', text: person.name },
+      { type: 'text', text: person.surname }
+    ]
+  }))
+]
 
 export default function HomePage() {
+  const [people] = useState<Person[]>(getPeople())
+
+  const rows = getRows(people)
+  const columns = getColumns()
+
   return (
-    <>
-      <Hero />
-      <Box sx={{ bgcolor: 'background.default' }}>
-        <Introduction />
-        <Footer />
+    <Wrapper>
+      <Typography variant="h4" fontWeight={'bold'}>
+        REACT GRID for Android
+      </Typography>
+      <Link
+        to="https://reactgrid.com/"
+        target="_blank"
+        style={{ color: 'inherit' }}
+      >
+        <Typography variant="h6">https://reactgrid.com/</Typography>
+      </Link>
+      <Box bgcolor={'white'} borderRadius={2} marginTop={2} p={2}>
+        <ReactGrid rows={rows} columns={columns} />
       </Box>
-    </>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+  marginTop: theme.spacing(5),
+  padding: theme.spacing(2)
+}))
--- src/router.tsx
+++ src/router.tsx
@@ -1,43 +1,10 @@
-import { Navigate, type RouteObject } from 'react-router'
-import CommitteePage from '@pages/CommitteePage'
+import { type RouteObject } from 'react-router'
 import HomePage from '@pages/HomePage'
-import KeynotePage from '@pages/KeynotePage'
-import Page404 from '@pages/Page404'
-import ProgramPage from '@pages/ProgramPage'
 
 const routes: RouteObject[] = [
   {
     path: '',
     element: <HomePage />
-  },
-  {
-    path: 'committee',
-    element: <CommitteePage />
-  },
-  {
-    path: 'keynote',
-    element: <KeynotePage />
-  },
-  {
-    path: 'keynote/:speaker',
-    element: <KeynotePage />
-  },
-  {
-    path: 'program',
-    element: <ProgramPage />
-  },
-  {
-    path: 'status',
-    children: [
-      {
-        path: '404',
-        element: <Page404 />
-      }
-    ]
-  },
-  {
-    path: '*',
-    element: <Navigate to="/status/404" replace />
   }
 ]
 
